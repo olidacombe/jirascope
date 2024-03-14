f8945aa1ae2ad3be51693526f50bb3e691f2fe75
Oli Dacombe <olidacombe@gmail.com>
2024-03-14 21:55:56 +0000

    start

---
+++ lua/jira/git.lua
@@ -0,0 +1,39 @@
+local Job = require("plenary.job")
+local M = {}
+
+M.branch_name = function()
+    return vim.system({ "git", "branch", "--show-current" }, { text = true }):wait().stdout:gsub("[\n\r]", "")
+end
+
+M.commit_message_from_branch_name = function(branch_name)
+    local message = branch_name:gsub("_", " ")
+    message = message:gsub("^%s*", "")
+    message = message:gsub("([%d%a]+%-[%d%a%-]+)", "`%1`")
+    return message
+end
+
+M.git_checkout_new_branch = function(default)
+    vim.ui.input({
+        prompt = "Branch Name",
+        default = default,
+    }, function(input)
+        if input then
+            Job:new({
+                command = "refalizer",
+                args = { input },
+                on_exit = function(j, _)
+                    Job:new({
+                        command = "git",
+                        args = {
+                            "checkout",
+                            "-b",
+                            j:result()[1],
+                        },
+                    }):start()
+                end,
+            }):start()
+        end
+    end)
+end
+
+return M
---
+++ lua/telescope/_extensions/jira.lua
@@ -0,0 +1,14 @@
+local has_telescope, telescope = pcall(require, "telescope")
+
+if not has_telescope then
+	error("jirascope requires nvim-telescope/telescope.nvim")
+end
+
+return telescope.register_extension({
+	-- setup = function(ext_config, config)
+	--     -- access extension config and user config
+	-- end,
+	exports = {
+		jira = require("jira").issues,
+	},
+})
